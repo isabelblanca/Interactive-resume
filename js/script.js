218133aaca8b5f03bfec8f6902996f5b7bec9866
isabelblanco <isabelblancodura@gmail.com>
2016-10-04 18:43:23 +0100

    Comments Review

--- js/script.js
+++ js/script.js
@@ -1,16 +1,6 @@
 /*
 
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
+This file contains all of the code running in the background that makes resumeBuilder.js possible.
 */
 
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
@@ -60,9 +50,6 @@ var internationalizeButton = '<button id="nameButton">Internationalize</button>'
 var googleMap = '<div id="map"></div>';
 
 
-/*
-The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
-*/
 $(document).ready(function() {
   $('button').click(function() {
     var iName = inName(name) || function(){};
@@ -71,7 +58,7 @@ $(document).ready(function() {
 });
 
 /*
-The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
+The next few lines about clicks are for the Collecting Click Locations.
 */
 clickLocations = [];
 
@@ -95,15 +82,14 @@ $(document).click(function(loc) {
 
 
 /*
-This is the fun part. Here's where we generate the custom Google Map for the website.
-See the documentation below for more details.
-https://developers.google.com/maps/documentation/javascript/reference
+Here's where we generate the custom Google Map for the website.
+
 */
 var map;    // declares a global map variable
 
 
 /*
-Start here! initializeMap() is called when page is loaded.
+initializeMap() is called when page is loaded.
 */
 function initializeMap() {
 
@@ -120,7 +106,7 @@ function initializeMap() {
   }
 
   // This next line makes `map` a new Google Map JavaScript Object and attaches it to
-  // <div id="map">, which is appended as part of an exercise late in the course.
+  // <div id="map">
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
 
@@ -171,9 +157,6 @@ function initializeMap() {
       title: name
     });
 
-    // infoWindows are the little helper windows that open when you click
-    // or hover over a pin on a map. They usually contain more information
-    // about a location.
     var infoWindow = new google.maps.InfoWindow({
       content: name
     });
@@ -240,9 +223,6 @@ function initializeMap() {
 
 }
 
-/*
-Uncomment the code below when you're ready to implement a Google Map!
-*/
 
 //Calls the initializeMap() function when the page loads
 window.addEventListener('load', initializeMap);
@@ -250,7 +230,6 @@ window.addEventListener('load', initializeMap);
 //Vanilla JS way to listen for resizing of the window
 //and adjust map bounds
 window.addEventListener('resize', function(e) {
- //Make sure the map bounds get updated on page resize
 map.fitBounds(mapBounds);
 google.maps.event.addDomListener(window, 'load', initialize);
 });
